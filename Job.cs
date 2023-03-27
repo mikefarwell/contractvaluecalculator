b887fb7be2ec24e89fd87e9be6e17d9cfd694d01
Mike Farwell <mikefarwell@gmail.com>
2023-03-27 10:14:49 -0500

    Update Job.cs

--- Job.cs
+++ Job.cs
@@ -1,12 +1,15 @@
-public class Job
+namespace ContractValueCalculator
 {
-    public string Classification { get; set; }
-    public string Grade { get; set; }
-    public double MinSalary { get; set; }
-    public double MaxSalary { get; set; }
-
-    public override string ToString()
+    public class Job
     {
-        return $"{Classification} ({Grade}) - {MinSalary:C} to {MaxSalary:C}";
+        public string Classification { get; set; }
+        public string Grade { get; set; }
+        public double MinSalary { get; set; }
+        public double MaxSalary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Classification} ({Grade}) - {MinSalary:C} to {MaxSalary:C}";
+        }
     }
 }

03badc5d8c88226d06f425e361d907c89a3c2efd
Mike Farwell <mikefarwell@gmail.com>
2023-03-27 10:10:00 -0500

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,9 @@
+public class Employee
+{
+    public string Name { get; set; }
+    public double Salary { get; set; }
+    public DateTime StartDate { get; set; }
+    public int VacationDays { get; set; }
+    public int SickDays { get; set; }
+    public int PersonalDays { get; set; }
+}

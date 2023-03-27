fe19c15433b23cadfd1d670f047ae5a8296e80d7
Mike Farwell <mikefarwell@gmail.com>
2023-03-27 10:14:13 -0500

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -1,9 +1,12 @@
-public class Employee
+namespace ContractValueCalculator
 {
-    public string Name { get; set; }
-    public double Salary { get; set; }
-    public DateTime StartDate { get; set; }
-    public int VacationDays { get; set; }
-    public int SickDays { get; set; }
-    public int PersonalDays { get; set; }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public DateTime StartDate { get; set; }
+        public int VacationDays { get; set; }
+        public int SickDays { get; set; }
+        public int PersonalDays { get; set; }
+    }
 }

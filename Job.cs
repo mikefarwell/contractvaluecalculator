7667958554811c0187c5bd49e44f60e023d5b57c
Mike Farwell <mikefarwell@gmail.com>
2023-03-27 10:10:38 -0500

    Create Job.cs

---
+++ Job.cs
@@ -0,0 +1,12 @@
+public class Job
+{
+    public string Classification { get; set; }
+    public string Grade { get; set; }
+    public double MinSalary { get; set; }
+    public double MaxSalary { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Classification} ({Grade}) - {MinSalary:C} to {MaxSalary:C}";
+    }
+}
